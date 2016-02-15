40f75c4c1174378ca3cdda9f8c4ca80f5a9ad99a
William Claude Tumeo <tumeowilliam@gmail.com>
2016-02-15 05:10:40 -0200

    add compiled

---
+++ scripts/script_temp.js
@@ -0,0 +1,28 @@
+var memberID = $_GET()["membros"];
+
+$(document).ready(
+    function () {
+        if(memberID !== undefined){
+            selectMember(memberID);
+        }
+    }
+);
+
+$('.select-member').click(
+    function (clickE) {
+        var memberID = this.getAttribute('member');
+        selectMember(memberID);
+    }
+);
+
+function selectMember(_id) {
+    $('.board').hide(0,
+        function () {
+            showMember(_id);
+        }
+    );
+}
+
+function showMember(_id){
+    $('.board-member-'+_id).show();
+}
